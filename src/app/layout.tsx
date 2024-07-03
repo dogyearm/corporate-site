338958d51f9245f97a37b531142a0f645c8fbda1
dogyearm <dogyear.iwmt@gmail.com>
2024-07-03 11:28:21 +0900

    微調整

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,11 +18,6 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content={metadata.description} />
-        <title>{metadata.title}</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="stylesheet" href={inter.href} />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
