e5c02a2669106f1f4a9af86e6220182836bcb21a
dogyearm <dogyear.iwmt@gmail.com>
2024-07-03 16:11:53 +0900

    微調整

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: process.env.BASE_PATH || "",
-  assetPrefix: process.env.BASE_PATH || "",
-  images: {
-    unoptimized: true,
+  publicRuntimeConfig: {
+    basePath: process.env.BASE_PATH || "",
   },
 };
 
