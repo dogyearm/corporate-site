20410023f26f498e3af0687af40d9c3b7278d300
dogyearm <dogyear.iwmt@gmail.com>
2024-07-03 11:54:42 +0900

    change base path

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
- output: "export",
+  output: "export",
+  basePath: "/corporate-site",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
