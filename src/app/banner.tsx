2e481fef1daa878fe4646b0a45a81eaf15f76605
dogyearm <dogyear.iwmt@gmail.com>
2024-07-04 11:40:00 +0900

    微調整

--- src/app/banner.tsx
+++ src/app/banner.tsx
@@ -3,12 +3,11 @@ import Link from 'next/link';
 
 export default function Bannar() {
   return (
-    <div className="relative flex flex-wrap bg-black px-4 py-1 sm:flex-nowrap sm:items-center sm:justify-center sm:gap-3 sm:pr-8 md:px-8">
-      <div className="order-1 mb-2 inline-block w-11/12 max-w-screen-sm text-sm text-white sm:order-none sm:mb-0 sm:w-auto md:text-base">
+    <div className="bg-black px-4 py-3 text-white">
+      <p className="text-center text-sm font-medium">
         Youtube チャネルにて開発事例公開中！
-      </div>
-
-      <a href="#" className="order-last inline-block w-full whitespace-nowrap rounded-lg bg-black px-4 py-2 text-center text-xs font-semibold text-white outline-none ring-slate-300 transition duration-100 hover:bg-slate-700 focus-visible:ring active:bg-slate-800 sm:order-none sm:w-auto md:text-sm">アクセスはこちらから →</a>
+        <a href="#" className="inline-block underline">Check out !!</a>
+      </p>
     </div>
   );
 }
