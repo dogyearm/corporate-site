a4405b7fdd026276340e4fdd4dfad2d9e70987ec
dogyearm <dogyear.iwmt@gmail.com>
2024-07-05 10:48:39 +0900

    add contact form

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -6,83 +6,126 @@ import { lulo } from './fonts';
 export default function Footer() {
   return (
     <footer className="bg-white">
-      <div className="pb-16 pt-4 sm:pt-10 lg:pt-12">
-        <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
-          <div className="grid grid-cols-2 gap-12 border-t pt-10 md:grid-cols-4 lg:grid-cols-6 lg:gap-8 lg:pt-12">
-            <div className="col-span-full lg:col-span-2">
-              <div className="mb-1 lg:-mt-2">
-                <a href="/" className="inline-flex items-center gap-1 text-sm font-bold text-black" aria-label="logo">
-                  <Image src="/logo.svg" width={40} height={40} alt="Neighbor Works" className="mb-2.5" />
-                  <span className={lulo.className}>Good Neighbor Works</span>
-                </a>
+      <section id="contact">
+        <div className="mx-auto max-w-screen-xl px-4 sm:px-16 lg:px-28">
+          <div className="lg:grid lg:grid-cols-2">
+            <div
+              className="border-b border-gray-400 py-8 lg:border-b-0 lg:border-e lg:py-16 lg:pe-16"
+            >
+              <div className="mt-8 space-y-4 lg:mt-0">
+                <span className="h-1 w-10 rounded bg-teal-500 block"></span>
+
+                <div>
+                  <h2 className={`${lulo.className} text-xl font-medium text-gray-900`}>Contact</h2>
+                  <p className="mt-4 max-w-lg text-gray-500">
+                  </p>
+                </div>
+
+                <div>
+                  <p>
+                    <span className="text-xs uppercase tracking-wide text-gray-500">お気軽にご相談ください</span>
+                    <a href="#" className="block text-2xl font-bold text-gray-600 hover:opacity-75 sm:text-xl">
+                      devlive@gmail.com
+                    </a>
+                  </p>
+
+                  <ul className="mt-8 flex gap-6">
+                    <li>
+                      <a
+                        href="#"
+                        rel="noreferrer"
+                        target="_blank"
+                        className="text-gray-700 transition hover:opacity-75"
+                      >
+                        <span className="sr-only">Instagram</span>
+
+                        <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                          <path
+                            fillRule="evenodd"
+                            d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+                      </a>
+                    </li>
+
+                    <li>
+                      <a
+                        href="#"
+                        rel="noreferrer"
+                        target="_blank"
+                        className="text-gray-700 transition hover:opacity-75"
+                      >
+                        <span className="sr-only">GitHub</span>
+
+                        <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                          <path
+                            fillRule="evenodd"
+                            d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
 
-            <div>
-              <div className="mb-4 font-bold text-sm uppercase tracking-widest text-gray-800">Products</div>
+            <div className="py-8 lg:py-16 lg:ps-16">
+              <form action="#" class="mt-8 grid grid-cols-6 gap-6">
+                <div class="col-span-6">
+                  <label for="Name" class="block text-sm font-medium text-gray-700">お名前</label>
 
-              <nav className="flex flex-col gap-4">
-                <div>
-                  <a href="#" className="text-sm text-gray-500 transition duration-100 hover:text-indigo-500 active:text-indigo-600">Works</a>
+                  <input
+                    type="text"
+                    id="Name"
+                    name="name"
+                    class="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm"
+                  />
                 </div>
 
-                <div>
-                  <a href="/" className="text-sm text-gray-500 transition duration-100 hover:text-indigo-500 active:text-indigo-600">Service</a>
-                </div>
-              </nav>
-            </div>
+                <div class="col-span-6">
+                  <label for="Email" class="block text-sm font-medium text-gray-700">メールアドレス</label>
 
-            <div>
-              <div className="mb-4 font-bold text-sm uppercase tracking-widest text-gray-800">Company</div>
-
-              <nav className="flex flex-col gap-4">
-                <div>
-                  <a href="/about" className="text-sm text-gray-500 transition duration-100 hover:text-indigo-500 active:text-indigo-600">About</a>
+                  <input
+                    type="email"
+                    id="Email"
+                    name="email"
+                    class="mt-1 w-full rounded-md border-gray-200 bg-white text-sm text-gray-700 shadow-sm"
+                  />
                 </div>
 
-                <div>
-                  <a href="/blog" className="text-sm text-gray-500 transition duration-100 hover:text-indigo-500 active:text-indigo-600">Blog</a>
+                <div class="col-span-6">
+                  <label for="Content" className="block text-sm font-medium text-gray-700">お問い合わせ内容</label>
+
+                  <textarea
+                    id="Content"
+                    className="mt-2 w-full rounded-lg border-gray-200 align-top shadow-sm sm:text-sm"
+                    rows="4"
+                    placeholder=""
+                  ></textarea>
                 </div>
-              </nav>
-            </div>
 
-            <div>
-              <div className="mb-4 font-bold text-sm uppercase tracking-widest text-gray-800">Support</div>
-
-              <nav className="flex flex-col gap-4">
-                <div>
-                  <a href="/contact" className="text-sm text-gray-500 transition duration-100 hover:text-indigo-500 active:text-indigo-600">Contact</a>
+                <div class="col-span-6">
+                  <p class="text-sm text-gray-500">
+                    <a href="#" class="underline">個人情報の取扱</a>
+                    に同意した上で送信してください。
+                  </p>
                 </div>
-              </nav>
-            </div>
-            <div>
-              <div className="mb-4 font-bold text-sm uppercase tracking-widest text-gray-800">Legal</div>
 
-              <nav className="flex flex-col gap-4">
-                <div>
-                  <a href="#" className="text-sm text-gray-500 transition duration-100 hover:text-indigo-500 active:text-indigo-600">Privacy Policy</a>
+                <div class="col-span-6 sm:flex sm:items-center sm:gap-4">
+                  <button
+                    class="inline-block shrink-0 rounded-md border border-gray-600 bg-gray-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-gray-600 focus:outline-none focus:ring active:text-gray-500"
+                  >
+                    送信する
+                  </button>
                 </div>
-              </nav>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="bg-gray-100">
-        <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
-          <div className="flex items-center justify-between gap-4 py-8">
-            <span className="text-sm text-gray-400"> © 2024 - Present Good Neighbor.inc All rights reserved. </span>
-
-            <div className="flex gap-4">
-              <a href="#" target="_blank" className="text-gray-400 transition duration-100 hover:text-gray-500 active:text-gray-600">
-                <svg className="h-5 w-5" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
-                </svg>
-              </a>
+              </form>
             </div>
           </div>
         </div>
-      </div>
+      </section>
     </footer>
   );
 }
