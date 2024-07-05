d8a87b1900bde9044245bf829c118d1e14fd98d1
dogyearm <dogyear.iwmt@gmail.com>
2024-07-05 16:24:17 +0900

    微調整

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   basePath: process.env.BASE_PATH || "",
   assetPrefix: process.env.BASE_PATH || "",
   publicRuntimeConfig: {
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content={metadata.description} />
+        <meta name="description" content={metadata.description ?? ""} />
         <title>{metadata.title}</title>
         <link rel="icon" href="/favicon.png" />
       </head>
