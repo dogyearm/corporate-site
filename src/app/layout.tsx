ee7bb51a0017aba20e80dd7646eb4dd3394ddd31
dogyearm <dogyear.iwmt@gmail.com>
2024-07-09 09:03:38 +0900

    remove unused component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { notojp } from "@fonts";
 
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
 import Transition from "@components/Transition";
 
 export const metadata: Metadata = {
