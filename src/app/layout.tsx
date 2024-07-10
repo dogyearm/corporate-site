11115c3a08684efc386a77687ca758ffb4946aa1
dogyearm <dogyear.iwmt@gmail.com>
2024-07-10 09:51:14 +0900

    change path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="https://goodneighbor.works/favicon.ico" />
       </head>
       <body className={notojp.className}>
         <Header />
